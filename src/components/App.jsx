f337bd7bb2cf33040b7e41ce51de64d0c37e8d14
VitaliiTymko <intehmet2015@gmail.com>
2023-01-11 13:44:20 +0200

    add css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import '../css/styles.css';
+
+export default class App extends Component {
+  state = {
+    photos: null,
+    page: 1,
+    value: '',
+  };
+
+  componentDidMount() {
+    fetch(
+      `https://pixabay.com/api/?q=${this.state.value}&page=${this.state.page}&key=31423589-05a77bf58d80d41712d5d29e1&image_type=photo&orientation=horizontal&per_page=12`
+    )
+      .then(res => res.json())
+      .then(photos => this.setState({ photos }));
+  }
+
+  render() {
+    console.log(this.state.photos);
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        {this.state.photos && (
+          <>
+            Тут будет разметка галлереи
+            <ul className="gallery">
+              {this.state.photos.hits.map(({ id, webformatURL, tags }) => (
+                <li key={id} className="gallery-item">
+                  <img src={webformatURL} alt={tags} />
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
+    );
+  }
+}
+
+//
